94d08619c87dd21aaad1fd2fc56ad342bf196c9d
James Dawson <63356869+polymurph13@users.noreply.github.com>
2020-06-22 17:26:41 +0100

    setup auth routes

--- models/User.model.js
+++ models/User.model.js
@@ -13,7 +13,7 @@ const userSchema = new Schema(
       required: true,
       unique: true
     },
-     passHash: {
+     passwordHash: {
       type: String,
       required: true
     },
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,134 +8,128 @@ const UserModel = require('../models/User.model');
 
 const { isLoggedIn } = require('../helpers/auth-helper'); // to check if user is loggedIn
 
-// router.post('/signup', (req, res) => {
-//     const {username, email, password } = req.body;
-//     console.log(username, email, password);
+router.post('/signup', (req, res) => {
+    const {username, email, password } = req.body;
+    console.log(username, email, password);
  
-//     if (!username || !email || !password) {
-//         res.status(500)
-//           .json({
-//             errorMessage: 'Please enter username, email and password'
-//           });
-//         return;  
-//     }
+    if (!username || !email || !password) {
+        res.status(500)
+          .json({
+            errorMessage: 'Please enter username, email and password'
+          });
+        return;  
+    }
 
-//     const myRegex = new RegExp(/^[a-z0-9](?!.*?[^\na-z0-9]{2})[^\s@]+@[^\s@]+\.[^\s@]+[a-z0-9]$/);
-//     if (!myRegex.test(email)) {
-//         res.status(500)
-//           .json({
-//             errorMessage: 'Email format not correct'
-//         });
-//         return;  
-//     }
+    const myRegex = new RegExp(/^[a-z0-9](?!.*?[^\na-z0-9]{2})[^\s@]+@[^\s@]+\.[^\s@]+[a-z0-9]$/);
+    if (!myRegex.test(email)) {
+        res.status(500)
+          .json({
+            errorMessage: 'Email format not correct'
+        });
+        return;  
+    }
 
-//     const myPassRegex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/);
-//     if (!myPassRegex.test(password)) {
-//       res.status(500)
-//           .json({
-//             errorMessage: 'Password needs to have 8 characters, a number and an Uppercase alphabet'
-//           });
-//         return;  
-//     }
+    const myPassRegex = new RegExp(/^(?=.*\d*)(?=.*[a-z]*)(?=.*[A-Z]*)(?=.*[a-zA-Z]*).{8,}$/);
+    if (!myPassRegex.test(password)) {
+      res.status(500)
+          .json({
+            errorMessage: 'Password needs to have at least 8 characters'
+          });
+        return;  
+    }
 
-//     bcrypt.genSalt(12)
-//       .then((salt) => {
-//         console.log('Salt: ', salt);
-//         bcrypt.hash(password, salt)
-//           .then((passwordHash) => {
-//             UserModel.create({email, username, passwordHash})
-//               .then((user) => {
-//                 user.passwordHash = "***";
-//                 req.session.loggedInUser = user;
-//                 console.log(req.session)
-//                 res.status(200).json(user);
-//               })
-//               .catch((err) => {
-//                 if (err.code === 11000) {
-//                   res.status(500)
-//                   .json({
-//                     errorMessage: 'username or email entered already exists!'
-//                   });
-//                   return;  
-//                 } 
-//                 else {
-//                   res.status(500)
-//                   .json({
-//                     errorMessage: 'Something went wrong! Go to sleep!'
-//                   });
-//                   return; 
-//                 }
-//               })
-//           });  
-//   });
+    bcrypt.genSalt(12)
+      .then((salt) => {
+        console.log('Salt: ', salt);
+        bcrypt.hash(password, salt)
+          .then((passwordHash) => {
 
-// });
+            // Create User
+            UserModel.create({email, username, passwordHash, dateJoined: new Date()})
+              .then((user) => {
+                user.passwordHash = "***";
+                req.session.loggedInUser = user;
+                console.log(req.session)
+                res.status(200).json(user);
+              })
+              .catch((err) => {
+                if (err.code === 11000) {
+                  res.status(500)
+                  .json({
+                    errorMessage: 'username or email entered already exists!'
+                  });
+                  return;  
+                } 
+                else {
+                  res.status(500)
+                  .json({
+                    errorMessage: 'Something went wrong creating user'
+                  });
+                  return; 
+                }
+              })
+          });  
+  });
+
+});
  
-// router.post('/signin', (req, res) => {
-//     const {email, password } = req.body;
-//     if ( !email || !password) {
-//         res.status(500).json({
-//             error: 'Please enter Username. email and password',
-//        })
-//       return;  
-//     }
-//     const myRegex = new RegExp(/^[a-z0-9](?!.*?[^\na-z0-9]{2})[^\s@]+@[^\s@]+\.[^\s@]+[a-z0-9]$/);
-//     if (!myRegex.test(email)) {
-//         res.status(500).json({
-//             error: 'Email format not correct',
-//         })
-//         return;  
-//     }
+router.post('/signin', (req, res) => {
+    const {username, password } = req.body;
+    if ( !username || !password) {
+        res.status(500).json({
+            error: 'Please enter username and password',
+       })
+      return;  
+    }
   
-//     // Find if the user exists in the database 
-//     UserModel.findOne({email})
-//       .then((userData) => {
-//            //check if passwords match
-//           bcrypt.compare(password, userData.passwordHash)
-//             .then((doesItMatch) => {
-//                 //if it matches
-//                 if (doesItMatch) {
-//                   // req.session is the special object that is available to you
-//                   user.passwordHash = "***";
-//                   req.session.loggedInUser = userData;
-//                   console.log('Signin', req.session)
-//                   res.status(200).json(userData)
-//                 }
-//                 //if passwords do not match
-//                 else {
-//                     res.status(500).json({
-//                         error: 'Passwords don\'t match',
-//                     })
-//                   return; 
-//                 }
-//             })
-//             .catch(() => {
-//                 res.status(500).json({
-//                     error: 'Email format not correct',
-//                 })
-//               return; 
-//             });
-//       })
-//       //throw an error if the user does not exists 
-//       .catch((err) => {
-//         res.status(500).json({
-//             error: 'Email format not correct',
-//             message: err
-//         })
-//         return;  
-//       });
+    // Find if the user exists in the database 
+    UserModel.findOne({username})
+      .then((userData) => {
+           //check if passwords match
+          bcrypt.compare(password, userData.passwordHash)
+            .then((isMatch) => {
+                if (isMatch) {
+                  // hide the user password hash in session
+                  userData.passwordHash = "***";
+                  req.session.loggedInUser = userData;
+                  console.log('Successfully signed in as ' + username);
+                  res.status(200).json(userData)
+                }
+                //if passwords do not match
+                else {
+                    res.status(500).json({
+                        error: 'Incorrect password, I\'m afraid',
+                    })
+                  return; 
+                }
+            })
+            .catch((err) => {
+                res.status(500).json({
+                    error: 'Failed to compare password and hash',
+                })
+              return; 
+            });
+      })
+      //throw an error if the user does not exists 
+      .catch((err) => {
+        res.status(500).json({
+            error: 'Hmm, that username isn\'t in the database, want to sign up with it?',
+            message: err
+        })
+        return;  
+      });
   
-// });
+});
  
-// router.post('/logout', (req, res) => {
-//     req.session.destroy();
-//     res
-//     .status(204) //  No Content
-//     .send();
-// })
+router.post('/logout', (req, res) => {
+    req.session.destroy();
+    res
+    .status(204) //  No Content
+    .send();
+})
 
-// router.get("/user", isLoggedIn, (req, res, next) => {
-//   res.status(200).json(req.session.loggedInUser);
-// });
+router.get("/user", isLoggedIn, (req, res, next) => {
+  res.status(200).json(req.session.loggedInUser);
+});
 
   module.exports = router;
